3e53b5fd8ff60e237d066b838c77e3528f0e07e8
kbadgery <33040070+kbadgery@users.noreply.github.com>
2018-08-13 13:04:23 -0600

    Update ANN.py
    Added random state seed to test/training split for consistency

--- ANN.py
+++ ANN.py
@@ -96,7 +96,7 @@ y=encoder_y.fit_transform(y)
 
 # Splitting the dataset
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
